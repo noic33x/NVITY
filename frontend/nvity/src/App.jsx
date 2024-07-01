a5a41c9f1e375af26a45ab87b7f9e233126f7c27
noic33x <radityaa256@gmail.com>
2024-07-01 09:22:26 +0800

    update BrowserRouter to HashRouter

--- frontend/nvity/src/App.jsx
+++ frontend/nvity/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import CreateTask from './components/CreateTask';
